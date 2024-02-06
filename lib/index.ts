9fe30dd32537641496ee571993a1e80f9dade99f
Bohdan Kontsedal <kontsedalbohdan@gmail.com>
2024-02-06 19:59:04 +0200

    add a cacheTime support

--- lib/index.ts
+++ lib/index.ts
@@ -8,12 +8,15 @@ export type UseQueryParams<T> = {
   key: string;
   getter: UseQueryGetter<T>;
   refetchInterval?: UseQueryRefetchInterval<T>;
+  cacheTime?: number;
 };
 
 export type QueryState<T> = {
   data: T | undefined;
   isLoading: boolean;
   error: Error | undefined;
+  cacheTime?: number;
+  lastAccessedAt?: number;
 };
 
 export type Cache = {
@@ -32,78 +35,119 @@ export type Cache = {
   initQueryParams: <T>(key: string) => void;
   getQueryParams: <T>(key: string) => QueryState<T> | undefined;
   fetchQuery: <T>(key: string, getter: () => Promise<T> | T) => Promise<void>
+  garbageCollectorInterval?: number | undefined;
+  toggleGarbageCollector: (enabled: boolean) => void;
 };
 
-export const createCache = (): Cache => ({
-  data: {},
-  listeners: {},
-  setQueryParams(key: string, values, notify = true) {
-    if (!this.data[key]) {
+export const createCache = () => {
+  const newCache: Cache = {
+    data: {},
+    listeners: {},
+    setQueryParams(key: string, values, notify = true) {
+      if (!this.data[key]) {
+        this.data[key] = {
+          data: undefined,
+          isLoading: false,
+          error: undefined,
+        };
+      }
       this.data[key] = {
-        data: undefined,
-        isLoading: false,
-        error: undefined,
+        ...this.data[key],
+        ...values,
       };
-    }
-    this.data[key] = {
-      ...this.data[key],
-      ...values,
-    };
-    if (notify && this.listeners[key]) {
+      if (notify && this.listeners[key]) {
+        try {
+          this.listeners[key].forEach((listener) => listener());
+        } catch (e) {
+          console.error(logPrefix, 'Error in listener', e);
+        }
+      }
+    },
+    subscribe(key, listener) {
+      if (!this.listeners[key]) {
+        this.listeners[key] = [];
+      }
+      this.listeners[key].push(listener);
+      return () => {
+        this.listeners[key] = this.listeners[key].filter((l) => l !== listener);
+      };
+    },
+    initQueryParams<T>(key: string, values?: Partial<QueryState<T>>) {
+      if (!this.data[key]) {
+        this.data[key] = {
+          data: undefined,
+          isLoading: false,
+          error: undefined,
+          cacheTime: 1000 * 60 * 5,
+          ...values,
+        };
+      }
+    },
+    getQueryParams<T>(key: string): QueryState<T> {
+      const result = this.data[key] as QueryState<T>;
+      if (result && typeof result.cacheTime === "number" && result.cacheTime > 0) {
+        this.setQueryParams(key, {
+          lastAccessedAt: Date.now()
+        }, false);
+      }
+      return result;
+    },
+
+    async fetchQuery<T>(key: string, getter: () => Promise<T> | T) {
+      this.initQueryParams(key);
+      const queryState = this.getQueryParams<T>(key);
+      if (!queryState || queryState.isLoading) {
+        return;
+      }
+      this.setQueryParams(key, {
+        isLoading: true,
+        error: undefined,
+      });
       try {
-        this.listeners[key].forEach((listener) => listener());
+        const result = await getter();
+        this.setQueryParams(key, {
+          data: result,
+          isLoading: false,
+          error: undefined,
+        });
       } catch (e) {
-        console.error(logPrefix, 'Error in listener', e);
+        this.setQueryParams(key, {
+          isLoading: false,
+          error: e as Error,
+        });
+      }
+    },
+    garbageCollectorInterval: undefined,
+    toggleGarbageCollector(enabled: boolean) {
+      if(enabled && !this.garbageCollectorInterval) {
+        this.garbageCollectorInterval = setInterval(() => {
+          const listenerKeys = Object.keys(this.listeners);
+          const now = Date.now();
+
+          listenerKeys.forEach(key => {
+            let data = this.data[key];
+            if(!data) {
+              return;
+            }
+            if(this.listeners[key].length > 0) {
+              return;
+            }
+            if(data.cacheTime && data.lastAccessedAt && now - data.lastAccessedAt > data.cacheTime) {
+              delete this.data[key];
+            }
+          })
+        }, 500)
+      }
+      if(!enabled && this.garbageCollectorInterval) {
+        clearInterval(this.garbageCollectorInterval)
+        this.garbageCollectorInterval = undefined;
       }
     }
-  },
-  subscribe(key, listener) {
-    if (!this.listeners[key]) {
-      this.listeners[key] = [];
-    }
-    this.listeners[key].push(listener);
-    return () => {
-      this.listeners[key] = this.listeners[key].filter((l) => l !== listener);
-    };
-  },
-  initQueryParams<T>(key: string) {
-    if (!this.data[key]) {
-      this.data[key] = {
-        data: undefined,
-        isLoading: false,
-        error: undefined,
-      };
-    }
-  },
-  getQueryParams<T>(key: string): QueryState<T> {
-    return this.data[key] as QueryState<T>;
-  },
+  };
 
-  async fetchQuery<T>(key: string, getter: () => Promise<T> | T) {
-    this.initQueryParams(key);
-    const queryState = this.getQueryParams<T>(key);
-    if (!queryState || queryState.isLoading) {
-      return;
-    }
-    this.setQueryParams(key, {
-      isLoading: true,
-      error: undefined,
-    });
-    try {
-      const result = await getter();
-      this.setQueryParams(key, {
-        data: result,
-        isLoading: false,
-        error: undefined,
-      });
-    } catch (e) {
-      this.setQueryParams(key, {
-        isLoading: false,
-        error: e as Error,
-      });
-    }
-  }
-})
+  newCache.toggleGarbageCollector(true);
+  return newCache;
+}
 export const globalCache: Cache = createCache();
 export const useQuery = <T>(params: UseQueryParams<T>, cache = globalCache) => {
   const initialQueryState = useRef(cache.getQueryParams<T>(params.key))
