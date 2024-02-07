d6635c6f33ca3288445bac8377d6e285d9c8b257
Kontsedal <kontsedalbohdan@gmail.com>
2024-02-07 22:59:58 +0200

    remove extra useMutation functionality

--- lib/useMutation.ts
+++ lib/useMutation.ts
@@ -1,26 +1,14 @@
 import { useMemo, useState } from "react";
-
-export type UseMutationOptions<T, D> = {
-  mutationFn: (vars: T) => Promise<D> | D;
-  onSuccess?: (data: D) => void;
-  onError?: (error: unknown) => void;
-};
-export const useMutation = <T, D>(params: UseMutationOptions<T, D>) => {
+export const useMutation = <T, D>(mutationFn: (vars: T) => Promise<D> | D) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<unknown | undefined>();
   const mutate = async (vars: T) => {
     setIsLoading(true);
     setError(undefined);
     try {
-      const result = await params.mutationFn(vars);
-      if (params.onSuccess) {
-        params.onSuccess(result);
-      }
+      return await mutationFn(vars);
     } catch (e) {
       setError(e);
-      if (params.onError) {
-        params.onError(e);
-      }
     }
     setIsLoading(false);
   };
