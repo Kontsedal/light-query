2cb9d91a07f3c9449ecde3fc7a5e50c7c6412925
Kontsedal <kontsedalbohdan@gmail.com>
2024-02-07 21:10:41 +0200

    improve package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
+import { esbuildPluginFilePathExtensions } from "esbuild-plugin-file-path-extensions";
 export default defineConfig({
-  entry: ['lib/index.ts'],
-  format: ['esm'],
-  outDir: 'dist',
+  entry: ["lib/index.ts"],
+  format: ["esm"],
+  outDir: "dist",
   dts: true,
   clean: true,
   minify: true,
+  esbuildPlugins: [esbuildPluginFilePathExtensions({ esmExtension: "js" })],
 });
