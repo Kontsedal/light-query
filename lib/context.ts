ca6fa17c6fa36e7aadbda98e54df0465ad7a5532
Kontsedal <kontsedalbohdan@gmail.com>
2024-02-09 19:03:57 +0200

    add useCache method

--- lib/context.ts
+++ lib/context.ts
@@ -1,3 +1,6 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { globalCache } from "./cache";
 export const CacheContext = createContext(globalCache);
+export const useCache = () => {
+  return useContext(CacheContext);
+};
