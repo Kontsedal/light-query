32a727220fad90fb6f4d40f0c5a3f5d0f7292b29
Kontsedal <kontsedalbohdan@gmail.com>
2024-03-15 13:42:00 +0200

    fest(): make mutation arguments not optional

--- lib/useMutation.ts
+++ lib/useMutation.ts
@@ -1,10 +1,10 @@
 import { useMemo, useState } from "react";
 import { useValueRef } from "./utils";
-export const useMutation = <T, D>(mutationFn: (vars?: T) => Promise<D> | D) => {
+export const useMutation = <T, D>(mutationFn: (vars: T) => Promise<D> | D) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<unknown | undefined>();
   const mutateRef = useValueRef(mutationFn);
-  const mutate = async (vars?: T) => {
+  const mutate = async (vars: T) => {
     setIsLoading(true);
     setError(undefined);
     try {
